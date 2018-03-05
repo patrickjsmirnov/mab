41c70e63c63a2fa8ee1c2efe911e2bea5e0ff599
Dmitriy Smirnov <you@example.com>
2018-03-06 00:39:52 +0300

    tabs

--- mab.py
+++ mab.py
@@ -1,3 +1,4 @@
+
 # import random
 import math
 # import numpy as np
@@ -34,49 +35,49 @@ def func_win():
 def func_first_win():
     for i in range(n):
         mean_win[i] = bernoulli.rvs(p1[i])
-	return 0
+    return 0
 
 
 def number_of_arms(t):
-	global current_number
-	max_temp = mean_win[0] / num_of_games[0] + math.sqrt(2 * math.log(t) / num_of_games[0])
-	max_temp_index = 0
-	for i in range(n):
-		temp[i] = mean_win[i] / num_of_games[i] + math.sqrt(2 * math.log(t) / num_of_games[i])
-		if temp[i] > max_temp:
-			max_temp = temp[i]
-			max_temp_index = i
-	current_number = max_temp_index
-	number_of_games[current_number] += 1
-	return 0
+    global current_number
+    max_temp = mean_win[0] / num_of_games[0] + math.sqrt(2 * math.log(t) / num_of_games[0])
+    max_temp_index = 0
+    for i in range(n):
+        temp[i] = mean_win[i] / num_of_games[i] + math.sqrt(2 * math.log(t) / num_of_games[i])
+        if temp[i] > max_temp:
+            max_temp = temp[i]
+            max_temp_index = i
+    current_number = max_temp_index
+    number_of_games[current_number] += 1
+    return 0
 
 
 def func_regret_calculation():
-	for i in range(time_finish):
-		j, temp_var = 0, 0
-		while j < i:
-			temp_var += p1[current_number_vector[j]]
-			j += 1
-		regret_vec[i] = i * p1[index_best_arm] - temp_var
-	return 0
+    for i in range(time_finish):
+        j, temp_var = 0, 0
+        while j < i:
+            temp_var += p1[current_number_vector[j]]
+            j += 1
+        regret_vec[i] = i * p1[index_best_arm] - temp_var
+    return 0
 
 
 # вычисляем доля оптимальных
 def func_per_of_optimal():
-	temp_vec = [0 for k in range(time_finish)]
-	i = 1
-	while i < time_finish:
-		if current_number_vector[i] == index_best_arm:
-			temp_vec[i] = 1
-		j = 0
-		s = 0
-		while j <= i:
-			s += temp_vec[j]
-			j += 1
-		per_of_optimal[i] = s / i
-		i += 1
+    temp_vec = [0 for k in range(time_finish)]
+    i = 1
+    while i < time_finish:
+        if current_number_vector[i] == index_best_arm:
+            temp_vec[i] = 1
+        j = 0
+        s = 0
+        while j <= i:
+            s += temp_vec[j]
+            j += 1
+        per_of_optimal[i] = s / i
+        i += 1
+    return 0
 
-	return 0
 
 #  без эксперта
 sum_of_reward = [0 for i in range(n)]
@@ -93,52 +94,52 @@ current_number_vector = [0 for i in range(time_finish)]
 temp = [0 for i in range(n)]
 
 while launch_count < launch_number:
-	count = 1
-	current_number = 0
-	for i in range(n):
-		mean_win[i] = 0
-		num_of_games[i] = 1
-		temp[i] = 0
-	for i in range(time_finish):
-		regret_vec[i] = 0
-		current_number_vector[i] = 0
-		per_of_optimal[i] = 0
+    count = 1
+    current_number = 0
+    for i in range(n):
+        mean_win[i] = 0
+        num_of_games[i] = 1
+        temp[i] = 0
+    for i in range(time_finish):
+        regret_vec[i] = 0
+        current_number_vector[i] = 0
+        per_of_optimal[i] = 0
 
-	func_first_win()
+    func_first_win()
 
-	while count < time_finish:
-		number_of_arms(count)
-		current_number_vector[count] = current_number
-		func_win()
-		count += 1
+    while count < time_finish:
+        number_of_arms(count)
+        current_number_vector[count] = current_number
+        func_win()
+        count += 1
 
-	func_per_of_optimal()
-	func_regret_calculation()
+    func_per_of_optimal()
+    func_regret_calculation()
 
-	for i in range(time_finish):
-		mean_regret_vec[i] += regret_vec[i]
-		sum_per_of_optimal[i] += per_of_optimal[i]
+    for i in range(time_finish):
+        mean_regret_vec[i] += regret_vec[i]
+        sum_per_of_optimal[i] += per_of_optimal[i]
 
-	for i in range(n):
-		sum_of_reward[i] += mean_win[i]
+    for i in range(n):
+        sum_of_reward[i] += mean_win[i]
 
-	launch_count += 1
+    launch_count += 1
 
 # суммарный выигрыш
 sum_reward = 0
 for i in range(n):
-	sum_reward += sum_of_reward[i] / launch_number
-	number_of_games[i] /= launch_number
-	sum_per_of_optimal[i] /= launch_number
+    sum_reward += sum_of_reward[i] / launch_number
+    number_of_games[i] /= launch_number
+    sum_per_of_optimal[i] /= launch_number
 
 
 print('sum_reward = ', sum_reward)
 print('curr_vec = ', number_of_games)
 
 for i in range(time_finish):
-	# sum_per_of_optimal[i] /= launch_number
-	mean_regret_vec[i] /= launch_number
-	# print('mean = ', mean_regret_vec_f[i])
+    # sum_per_of_optimal[i] /= launch_number
+    mean_regret_vec[i] /= launch_number
+    # print('mean = ', mean_regret_vec_f[i])
 
 # считаем долю оптимальных
 # sum_optimal = 0
@@ -148,25 +149,23 @@ for i in range(time_finish):
 
 
 
-
 def upper_bound():
-	delta = [0 for i in range(n)]
-	temp2 = 0
-	for i in range(n):
-		if p1[i] < p1[index_best_arm]:
-			delta[i] = p1[index_best_arm] - p1[i]
-		temp2 += delta[i]
+    delta = [0 for i in range(n)]
+    temp2 = 0
+    for i in range(n):
+        if p1[i] < p1[index_best_arm]:
+            delta[i] = p1[index_best_arm] - p1[i]
+        temp2 += delta[i]
 
-	i = 1
-	while i < time_finish:
-		temp1 = 0
-		for k in range(n):
-			if delta[k] != 0:
-				temp1 += 8 * math.log(i) / delta[k]
-		upper_bound_vector[i] = temp1 + (1 + math.pi * math.pi / 3) * temp2
-		i += 1
-
-	return 0
+    i = 1
+    while i < time_finish:
+        temp1 = 0
+        for k in range(n):
+            if delta[k] != 0:
+                temp1 += 8 * math.log(i) / delta[k]
+            upper_bound_vector[i] = temp1 + (1 + math.pi * math.pi / 3) * temp2
+        i += 1
+    return 0
 
 
 time = [i for i in range(time_finish)]
