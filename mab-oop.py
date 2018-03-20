645f3f8a52c520e5977e9f40539df8c4adcc0e67
Dmitriy Smirnov <patrickjsmirnov@gmail.com>
2018-03-20 22:35:04 +0300

    pursuit algorithm added

--- mab-oop.py
+++ mab-oop.py
@@ -19,12 +19,16 @@ class Mab:
         self.current_index = 0
         self.total_number_of_games = self.n
         self.current_index_vector = []
+        self.pursuit_probability_array = []
 
     def get_max_of_probability_vector(self):
         return max(self.probability_vector)
 
     def get_index_of_max_of_probably_vector(self):
         return self.probability_vector.index(max(self.probability_vector))
+
+    def get_index_of_max_of_mean_win_value(self):
+        return self.mean_win_value.index(max(self.mean_win_value))
 
     def play(self):
         self.win_value[self.current_index] += bernoulli.rvs(self.probability_vector[self.current_index])
@@ -77,6 +81,32 @@ class Mab:
             index += 1
             probability_sum += softmax_probability_array[index]
 
+    def pursuit(self, beta, t):
+        if t == 0:
+            self.pursuit_probability_array = [1/self.n for i in range(self.n)]
+            return
+
+        index_max_mean_win_value = self.get_index_of_max_of_mean_win_value()
+
+        for i in range(self.n):
+            if i == index_max_mean_win_value:
+                self.pursuit_probability_array[i] += beta * (1 - self.pursuit_probability_array[i])
+                continue
+
+            self.pursuit_probability_array[i] -= beta * self.pursuit_probability_array[i]
+
+        x = random.random()
+        probability_sum = self.pursuit_probability_array[0]
+        index = 0
+
+        while index < self.n:
+            if x < probability_sum:
+                self.current_index = index
+                break
+
+            index += 1
+            probability_sum += self.pursuit_probability_array[index]
+
     def get_regret(self):
         regret_vector = []
         for i in range(self.horizon):
@@ -103,11 +133,11 @@ class Mab:
 
 
 horizon = 10000
-mab = Mab([0.1, 0.3, 0.2], horizon)
+mab = Mab([0.1, 0.3, 0.2, 0.4, 0.45, 0.39, 0.6], horizon)
 
 i = 0
 while i < horizon:
-    mab.softmax(0.01)
+    mab.pursuit(0.001, i)
     mab.play()
     i += 1
 
